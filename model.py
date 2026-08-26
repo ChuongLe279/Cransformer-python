e0ab5541abcd8d0a5df34ff223715fb1e27a47c1
ChuongLe279 <lebuuchuong2024@gmail.com>
2026-08-26 22:51:17 +0700

    input embedding

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbedding(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocal_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+
+if __name__ == "__main__":
+    input_embedding = InputEmbedding(512, 26).embedding
+    print(input_embedding)
