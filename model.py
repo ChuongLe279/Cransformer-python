d591f9574d089417f416d9b0a1f9b9383f2e6eea
ChuongLe279 <lebuuchuong2024@gmail.com>
2026-08-29 12:24:42 +0700

    Feed forward

--- model.py
+++ model.py
@@ -55,6 +55,17 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dims = -1, keepdim = True) # (batch, seq_len, 1)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
 
 if __name__ == "__main__":
     model = PositionalEncoding(d_model=10, seq_length=10, dropout=0.0)
